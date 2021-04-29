67fdcb1124d469fae5febb65887a8c0b4f218e6c
Juneus Magsakay <jbm9397@gmail.com>
2021-04-29 15:44:25 -0400

    initial commit

---
+++ java.js
@@ -0,0 +1,9 @@
+function hide() {
+    var x = document.getElementById("hide");
+    x.style.display = "none";
+    }
+
+    function hide2() {
+        var x = document.getElementById("hide2");
+        x.style.display = "none";
+        }
